5f18d21bbd986ad999ffad80c69878ce008eb4cd
Paulo Tomé Gomes de Vargas <paulotomegomesdevargas@gmail.com>
2023-11-24 13:38:21 -0300

    Estilizando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>{{ title }}</h1>
-    <app-task-list></app-task-list>
+    <h1 class="text-center">{{ title }}</h1>
+    <app-task-list class="justify-content-center"></app-task-list>
   `,
   styleUrls: ['./app.component.css']
 })
